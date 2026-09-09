94c9b3e9ebae21b2d6167198751053d2c1eedd36
adrianverdu59-lgtm <adrianverdu59@gmail.com>
2026-09-09 15:52:26 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'features/auth/login_screen.dart';
 
-void main() {
-  runApp(const SuperCrediApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://rnndnecnmlojydtmtvun.supabase.co',
+    anonKey: 'AQUI_PEGA_TU_CLAVE',
+  );
+
+  runApp(const MyApp());
 }
 
-class SuperCrediApp extends StatelessWidget {
-  const SuperCrediApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Super Credi',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF0D0F14),
-        primaryColor: const Color(0xFF00A3FF),
-        cardColor: const Color(0xFF161B22),
-        colorScheme: const ColorScheme.dark(
-          primary: Color(0xFF00A3FF),
-          secondary: Color(0xFF00E5FF),
-          surface: Color(0xFF161B22),
-        ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
       home: const LoginScreen(),
     );
